f5cc8ff0b95a682b48bf05d1403d5c96eb2dfc66
Max Pfister <pfister_max@web.de>
2025-03-23 15:10:49 +0100

    adds content for pages

--- pages/players.php
+++ pages/players.php
@@ -1,1 +1,21 @@
-<h1>Hello players</h1>
+<?php
+
+include './../helper.php';
+
+$config = loadConfig();
+$allDevices = fetchAllDevices($config);
+
+foreach ($allDevices as $device):
+    $deviceType = explode(".", $device['entity_id'])[0];
+    if ($deviceType !== "media_player") 
+        continue;
+
+    $serviceParam = $device['state'] == "off" ? 'on' : 'off';
+?>
+<div class="device-box" id="<?= $device['entity_id'] ?>">
+    <a href="<?= "javascript:stateChange('" . $device['entity_id'] . "', 'turn_" . $serviceParam . "')" ?>">
+        <?= getIconFile($deviceType, $device['state']) ?>
+        <p><?= $device['attributes']['friendly_name'] ?></p>
+    </a>
+</div>
+<?php endforeach; ?>
--- pages/switches.php
+++ pages/switches.php
@@ -1,1 +1,28 @@
-<h1>Hello Switches</h1>
+<?php
+
+include './../helper.php';
+
+$config = loadConfig();
+$allDevices = fetchAllDevices($config);
+
+usort($allDevices, function($a, $b) {
+    return strcmp(
+        strtolower($a['attributes']['friendly_name'] ?? ''),
+        strtolower($b['attributes']['friendly_name'] ?? '')
+    );
+});
+
+foreach ($allDevices as $device):
+    $deviceType = explode(".", $device['entity_id'])[0];
+    if ($deviceType !== "switch") 
+        continue;
+
+    $serviceParam = $device['state'] == "off" ? 'on' : 'off';
+?>
+<div class="device-box" id="<?= $device['entity_id'] ?>">
+    <a href="<?= "javascript:stateChange('" . $device['entity_id'] . "', 'turn_" . $serviceParam . "')" ?>">
+        <?= getIconFile($deviceType, $device['state']) ?>
+        <p><?= $device['attributes']['friendly_name'] ?></p>
+    </a>
+</div>
+<?php endforeach; ?>
